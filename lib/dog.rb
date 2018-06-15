62603099c4bc32bdda21f7d8838d16f16d8b0acf
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 23:37:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
-      def name(name)
+      def initialize(name)
             @name = name
       end
-      # def name= (name)
-      #       @name = name
-      # end
-      # def name
-      #       @name
-      # end
-      def breed(breed = "Mutt")
+      def name= (name)
+            @name = name
+      end
+      def name
+            @name
+      end
+      def initialize(breed = "Mutt")
             @breed = breed
       end
-      # def breed= (breed)
-      #       @breed = breed
-      # end
-      # def breed
-      #       @breed
-      # end
+      def breed= (breed)
+            @breed = breed
+      end
+      def breed
+            @breed
+      end
 end
