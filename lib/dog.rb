f3125a5e277c5f00c67e196cbad2aa1d52029e7e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 23:36:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,19 +2,19 @@ class Dog
       def name(name)
             @name = name
       end
-      def name= (name)
-            @name = name
-      end
-      def name
-            @name
-      end
+      # def name= (name)
+      #       @name = name
+      # end
+      # def name
+      #       @name
+      # end
       def breed(breed = "Mutt")
             @breed = breed
       end
-      def breed= (breed)
-            @breed = breed
-      end
-      def breed
-            @breed
-      end
+      # def breed= (breed)
+      #       @breed = breed
+      # end
+      # def breed
+      #       @breed
+      # end
 end
